a430829aa128aa8a7a3c9c2e2a92fbcece079a28
AAT <AAT@example.com>
2024-06-19 13:36:29 -0800

    Final commit

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import requests
 import selectorlib
 from send_email import send_email
 import time
+import sqlite3
 
 URL = "https://programmer100.pythonanywhere.com/tours"
+
+
+connection = sqlite3.connect("data.db")
+cursor = connection.cursor()
 
 def scrape(url):
     """ Scrape the page source from URL """
@@ -18,22 +23,31 @@ def extract(source):
 
 
 def store(extracted):
-    with open("data.txt", "a") as file:
-        file.write(extracted + "\n")
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+    connection.commit()
 
 def read(extracted):
-    with open("data.txt", "r") as file:
-        return file.read()
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    band, city, date = row
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
+    rows = cursor.fetchall()
+    print(rows)
+    return rows
 
 if __name__ == "__main__":
     while True:
         scraped = scrape(URL)
         extracted = extract(scraped)
         print(extracted)
-        content = read(extracted)
         
         if extracted != "No upcoming tours":
-            if extracted not in content:
+            row = read(extracted)
+            if not row:
                 store(extracted)
                 send_email()
         time.sleep(2)
